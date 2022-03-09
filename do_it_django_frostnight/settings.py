691e3d4fd65b0da837ccfcff811b017d3d8e46a6
frostnight <frostnightsong@gmail.com>
2022-03-09 17:26:48 +0900

    미디어 파일 관리하기 - 이미지 파일 업로드를 위한 ImageField

--- do_it_django_frostnight/settings.py
+++ do_it_django_frostnight/settings.py
@@ -135,6 +135,13 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
