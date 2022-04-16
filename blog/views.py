82027afb6eb6a4a1d284a2babedbd98c38d03e68
frostnight <frostnightsong@gmail.com>
2022-04-16 17:40:00 +0900

    카테고리 페이지 마무리

--- blog/views.py
+++ blog/views.py
@@ -28,14 +28,20 @@ class PostDetail(DetailView):
 
 
 def category_page(request, slug):
-    category = Category.objects.get(slug=slug)
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
     return render(
         request,
         'blog/post_list.html',
         {
-            'post_list': Post.objects.filter(category=category),
+            'post_list': post_list,
             'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None)
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category
         }
     )
