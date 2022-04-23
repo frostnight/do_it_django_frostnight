6d4a1a5fc807e3281bc7c09b15c27fc0e341291c
frostnight <frostnightsong@gmail.com>
2022-04-23 14:23:43 +0900

    로그인한 사용자에게만 포스트 작성하기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView
 
 from blog.models import Post, Category, Tag
@@ -27,11 +26,17 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
 
-
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 def category_page(request, slug):
     if slug == 'no_category':
