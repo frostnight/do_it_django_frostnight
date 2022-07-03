0040c1da46bc1c0406e1b0e932b22a316882cf3f
frostnight <frostnightsong@gmail.com>
2022-07-03 18:51:01 +0900

    도커파일과 도커 컴포즈 파일 만들기

--- do_it_django_frostnight/settings.py
+++ do_it_django_frostnight/settings.py
@@ -37,13 +37,16 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", 1)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(" ")
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
