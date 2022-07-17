6730f2692f2bc090c81639d2e10be044b6309a20
frostnight <frostnightsong@gmail.com>
2022-07-17 16:12:48 +0900

    docker 웹서버 Nginx 설정

--- do_it_django_frostnight/settings.py
+++ do_it_django_frostnight/settings.py
@@ -151,10 +151,6 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
