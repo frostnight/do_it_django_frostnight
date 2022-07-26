35ebe2abf078a06837e7414de0ace1bd2d5547d5
frostnight <frostnightsong@gmail.com>
2022-07-27 04:52:18 +0900

    board create

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('create/', views.BoardCreate.as_view()),
     path('<int:pk>/', views.BoardDetail.as_view()),
     path('', views.BoardList.as_view()),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView
 
 from board.models import Board
 
@@ -7,5 +9,21 @@ class BoardList(ListView):
     model = Board
     ordering = '-pk'
 
+
 class BoardDetail(DetailView):
     model = Board
+
+
+class BoardCreate(LoginRequiredMixin, CreateView):
+    model = Board
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            response = super(BoardCreate, self).form_valid(form)
+
+            return response
+        else:
+            return redirect('/board/')
